75bbd9cac88de03ab987717b4dc2876ba939e1ac
alexander_ivanov@brown.edu <alexander_ivanov@brown.edu>
2025-07-26 21:13:46 -0400

    update lobby

--- main.gd
+++ main.gd
@@ -6,22 +6,23 @@ var InputBox : TextEdit = $InputBox
 @onready
 var ChatLog : RichTextLabel = $ChatLog
 
-var lobby : Lobby = Lobby.new()
+@onready
+var Lobby : Lobby = $LobbyNode
 
 func _ready() -> void:
 	print("Ready!")
-	lobby.player_connected.connect(_player_connected)
-	lobby.player_disconnected.connect(_player_disconnected)
-	lobby.server_disconnected.connect(_server_disconnected)
+	Lobby.player_connected.connect(_player_connected)
+	Lobby.player_disconnected.connect(_player_disconnected)
+	Lobby.server_disconnected.connect(_server_disconnected)
 	
 #func _process(delta: float) -> void:
 	#print("tick" + str(delta))
 
 func _on_create_game_pressed() -> void:
-	lobby.create_game(int($Create/PORT.text))
+	Lobby.create_game(int($Create/PORT.text))
 	
 func _on_join_game_pressed() -> void:
-	lobby.join_game($Join/IP.text, int($Join/PORT.text))
+	Lobby.join_game($Join/IP.text, int($Join/PORT.text))
 
 func _on_update_name_pressed() -> void:
 	pass # Replace with function body.
