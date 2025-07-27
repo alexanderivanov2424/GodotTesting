d3f49609106d84c2af432f15cdfd4635eb244c6d
alexander_ivanov@brown.edu <alexander_ivanov@brown.edu>
2025-07-26 20:08:02 -0400

    stub functions

--- main.gd
+++ main.gd
@@ -6,6 +6,19 @@ var InputBox : TextEdit = $InputBox
 @onready
 var ChatLog : RichTextLabel = $ChatLog
 
-
 func _ready() -> void:
 	print("Ready!")
+	print(InputBox.text)
+	
+func create_server(PORT : int, MAX_CLIENTS : int) -> void:
+	var peer = ENetMultiplayerPeer.new()
+	peer.create_server(PORT, MAX_CLIENTS)
+	self.multiplayer.multiplayer_peer = peer
+	
+func create_client(IP_ADDRESS : String, PORT : int) -> void:
+	var peer = ENetMultiplayerPeer.new()
+	peer.create_client(IP_ADDRESS, PORT)
+	multiplayer.multiplayer_peer = peer
+	
+func _process(delta: float) -> void:
+	print("tick" + str(delta))
