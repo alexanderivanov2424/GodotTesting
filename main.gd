e420588c41307e19a6f8d8258a187c338ff26d49
alexander_ivanov@brown.edu <alexander_ivanov@brown.edu>
2025-07-26 20:26:02 -0400

    add signal

--- main.gd
+++ main.gd
@@ -10,15 +10,11 @@ func _ready() -> void:
 	print("Ready!")
 	print(InputBox.text)
 	
-func create_server(PORT : int, MAX_CLIENTS : int) -> void:
-	var peer = ENetMultiplayerPeer.new()
-	peer.create_server(PORT, MAX_CLIENTS)
-	self.multiplayer.multiplayer_peer = peer
-	
-func create_client(IP_ADDRESS : String, PORT : int) -> void:
-	var peer = ENetMultiplayerPeer.new()
-	peer.create_client(IP_ADDRESS, PORT)
-	multiplayer.multiplayer_peer = peer
-	
 func _process(delta: float) -> void:
 	print("tick" + str(delta))
+
+
+func _on_create_game_pressed() -> void:
+	pass # Replace with function body.
+	
+	
