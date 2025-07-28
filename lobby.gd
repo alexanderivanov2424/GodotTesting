5b676d3393682347d2b3d5a6b27d7d037eacb51d
Jason Crowley <crowley.json@gmail.com>
2025-07-27 19:06:00 -0700

    style

--- lobby.gd
+++ lobby.gd
@@ -3,40 +3,39 @@ extends Node
 # Autoload named Lobby
 
 # These signals can be connected to by a UI lobby scene or the game scene.
-signal player_connected(peer_id : int, player_info)
-signal player_disconnected(peer_id : int)
+signal player_connected(peer_id: int, player_info: Dictionary)
+signal player_disconnected(peer_id: int)
 signal server_disconnected
-
-signal message_received(player_info, message : String)
+signal message_received(player_info: Dictionary, message: String)
 
 const MAX_CONNECTIONS = 20
+const SERVER_ID = 1
 
 # This will contain player info for every player,
 # with the keys being each player's unique IDs.
-var players = {}
+var players: Dictionary[int, Dictionary] = {}
 
-var player_info = {"name" : "Name"}
+# This is the local player info.
+var player_info: Dictionary = {"name": "Name"}
 
 func _ready():
 	multiplayer.peer_connected.connect(_on_player_connected)
 	multiplayer.peer_disconnected.connect(_on_player_disconnected)
-	
+
 	multiplayer.connected_to_server.connect(_on_connected_ok)
 	multiplayer.connection_failed.connect(_on_connected_fail)
 	multiplayer.server_disconnected.connect(_on_server_disconnected)
 
-func join_game(IP : String, PORT : int):
-	var peer = ENetMultiplayerPeer.new()
-	var error = peer.create_client(IP, PORT)
+func join_game(ip: String, port: int):
+	var peer := ENetMultiplayerPeer.new()
+	var error := peer.create_client(ip, port)
 	if error:
 		return error
 	multiplayer.multiplayer_peer = peer
 
-const SERVER_ID = 1
-
-func create_game(PORT : int):
-	var peer = ENetMultiplayerPeer.new()
-	var error = peer.create_server(PORT, MAX_CONNECTIONS)
+func create_game(port: int):
+	var peer := ENetMultiplayerPeer.new()
+	var error := peer.create_server(port, MAX_CONNECTIONS)
 	if error:
 		return error
 	multiplayer.multiplayer_peer = peer
@@ -50,21 +49,21 @@ func remove_multiplayer_peer():
 
 # When a peer connects, send them my player info.
 # This allows transfer of all desired data for each player, not only the unique ID.
-func _on_player_connected(id : int):
+func _on_player_connected(id: int):
 	_register_player.rpc_id(id, player_info)
 
 @rpc("any_peer", "reliable")
-func _register_player(new_player_info):
-	var new_player_id = multiplayer.get_remote_sender_id()
+func _register_player(new_player_info: Dictionary):
+	var new_player_id := multiplayer.get_remote_sender_id()
 	players[new_player_id] = new_player_info
 	player_connected.emit(new_player_id, new_player_info)
 
-func _on_player_disconnected(id):
+func _on_player_disconnected(id: int):
 	players.erase(id)
 	player_disconnected.emit(id)
 
 func _on_connected_ok():
-	var peer_id = multiplayer.get_unique_id()
+	var peer_id := multiplayer.get_unique_id()
 	players[peer_id] = player_info
 	player_connected.emit(peer_id, player_info)
 
@@ -75,25 +74,24 @@ func _on_server_disconnected():
 	multiplayer.multiplayer_peer = null
 	players.clear()
 	server_disconnected.emit()
-	
-	
+
 # ########################
 
-func send_message(message : String):
-	var peer_id : int = multiplayer.get_unique_id()
+func send_message(message: String):
+	var peer_id := multiplayer.get_unique_id()
 	_send_message.rpc(peer_id, message)
 
 @rpc("any_peer", "call_local", "reliable")
-func _send_message(peer_id, message : String):
-	var sender = players[peer_id]
+func _send_message(peer_id: int, message: String):
+	var sender := players[peer_id]
 	message_received.emit(sender, message)
-	
-func update_name(new_name : String):
+
+func update_name(new_name: String):
 	player_info["name"] = new_name
-	var peer_id : int = multiplayer.get_unique_id()
+	var peer_id := multiplayer.get_unique_id()
 	_update_name.rpc(peer_id, new_name)
 
 @rpc("any_peer", "call_local", "reliable")
-func _update_name(peer_id : int, new_name : String):
+func _update_name(peer_id: int, new_name: String):
 	if peer_id in players:
 		players[peer_id]["name"] = new_name
--- main.gd
+++ main.gd
@@ -1,30 +1,25 @@
 extends Node
 
-@onready
-var InputBox : TextEdit = $InputBox
+var logs: Array[String] = []
 
-@onready
-var ChatLog : RichTextLabel = $ChatLog
-
-@onready
-var Lobby : Node = $Lobby
-
-var logs : Array[String] = []
+@onready var input: TextEdit = $InputBox
+@onready var chat: RichTextLabel = $ChatLog
+@onready var Lobby: Node = $Lobby
 
 func _ready() -> void:
 	Lobby.player_connected.connect(_player_connected)
 	Lobby.player_disconnected.connect(_player_disconnected)
 	Lobby.server_disconnected.connect(_server_disconnected)
-	
+
 	Lobby.message_received.connect(_got_message)
-	
+
 #func _process(delta: float) -> void:
 	#print("tick" + str(delta))
 
 func _on_create_game_pressed() -> void:
 	Lobby.create_game(int($Create/PORT.text))
 	print("Server Created")
-	
+
 func _on_join_game_pressed() -> void:
 	Lobby.join_game($Join/IP.text, int($Join/PORT.text))
 	print("Server Joined")
@@ -34,22 +29,20 @@ func _on_update_name_pressed() -> void:
 	print("Name Updated")
 
 
-func _player_connected(peer_id : int, player_info):
-	print(str(peer_id) + " " + player_info["name"] + " connected!")
-	
-func _player_disconnected(peer_id : int):
-	print(str(peer_id) + " disconnected!")
-	
+func _player_connected(peer_id: int, player_info: Dictionary):
+	print("%d %s connected!" % [peer_id, player_info["name"]])
+
+func _player_disconnected(peer_id: int):
+	print("%d disconnected!" % peer_id)
+
 func _server_disconnected():
 	print("server closed")
-	
-func _got_message(player_info, message : String):
-	var message_line = "[ {player_name} ] {message} \n"
-	var formatting = {"player_name" : player_info["name"],	"message" : message}
-	ChatLog.text += message_line.format(formatting)
-	ChatLog.scroll_to_line(ChatLog.get_line_count())
 
-func _on_send_button_pressed() -> void:
-	logs.append(InputBox.text)
-	Lobby.send_message(InputBox.text)
-	InputBox.clear()
+func _got_message(player_info: Dictionary, message: String):
+	chat.text += "[ %s ] %s \n" % [player_info["name"], message]
+	chat.scroll_to_line(chat.get_line_count())
+
+func _on_send_button_pressed():
+	logs.append(input.text)
+	Lobby.send_message(input.text)
+	input.clear()
