6e20f7bc7695a8eed9071802cc841f641384fc47
Jason Crowley <crowley.json@gmail.com>
2025-08-03 15:36:17 -0700

    auto spawn/sync players and remove logs

--- scripts/game.gd
+++ scripts/game.gd
@@ -3,25 +3,17 @@ extends Node
 @export var Map: PackedScene
 @export var Player: PackedScene
 
-@onready var spawner: MultiplayerSpawner = $Spawner
 @onready var spawn_point: Node = $SpawnPoint
 
 func _ready() -> void:
 	Lobby.lobby_ready.connect(_start_game)
 
-	spawner.spawn_function = _spawn_player
-
 func _start_game():
-	for c in spawn_point.get_children():
-		spawn_point.remove_child(c)
-		c.queue_free()
-
 	spawn_point.add_child(Map.instantiate())
-	for peer_id in Lobby.players:
-		spawner.spawn(peer_id)
 
-func _spawn_player(data: Variant) -> Node:
-	var peer_id := int(data)
-	var player: Player = Player.instantiate()
-	player.set_peer_id(peer_id)
-	return player
+	for peer_id in Lobby.players:
+		var player := Player.instantiate()
+		player.id = peer_id
+
+		spawn_point.add_child(player, true)
+		player.set_multiplayer_authority(peer_id)
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,26 +1,23 @@
 extends CharacterBody2D
-class_name Player
 
 @export var speed := 400
-@export var peer_id := -1
+
+# Set by the authority, synchronized on spawn.
+@export var id: int = -1:
+	set(peer_id):
+		id = peer_id
+		$ID.text = str(peer_id)
 
 @onready var camera: Camera2D = $Camera2D
 
 func _ready() -> void:
-	print("ready")
-	if peer_id == multiplayer.get_unique_id():
+	if id == multiplayer.get_unique_id():
 		camera.make_current()
 
 func _physics_process(_delta):
-	if peer_id == multiplayer.get_unique_id():
+	if id == multiplayer.get_unique_id():
 		get_input()
 		move_and_slide()
-
-func set_peer_id(peer_id: int):
-	print("setting %d" % peer_id)
-	self.peer_id = peer_id
-	$ID.text = str(peer_id)
-	set_multiplayer_authority(peer_id)
 
 func get_input():
 	var input_direction := Input.get_vector("left", "right", "up", "down")
