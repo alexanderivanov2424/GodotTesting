0fcaffe0395f327e8001d37e87777c83cd245e74
alexander_ivanov@brown.edu <alexander_ivanov@brown.edu>
2025-08-02 23:04:07 -0400

    adding spawn function

--- scripts/game.gd
+++ scripts/game.gd
@@ -7,19 +7,32 @@ extends Node
 func _ready() -> void:
 	Lobby.lobby_ready.connect(_start_game)
 	
-	$MultiplayerSpawner.add_spawnable_scene(game_scene.resource_path)
-	$MultiplayerSpawner.add_spawnable_scene(player_scene.resource_path)
+	$MapSpawner.add_spawnable_scene(game_scene.resource_path)
+	$PlayerSpawner.add_spawnable_scene(player_scene.resource_path)
+	
+	$PlayerSpawner.spawn_function = spawn_player
 
 func _start_game():	
-	var multiplayer_root = $MultiplayerRoot
+	var map_root = $Map
+	var player_root = $Players
 	
-	for c in multiplayer_root.get_children():
-		multiplayer_root.remove_child(c)
+	for c in map_root.get_children():
+		map_root.remove_child(c)
 		c.queue_free()
 		
-	multiplayer_root.add_child(game_scene.instantiate())
+	map_root.add_child(game_scene.instantiate())
+		
+	for c in player_root.get_children():
+		player_root.remove_child(c)
+		c.queue_free()
+	
 	
 	for peer_id in Lobby.players:
-		var player : Player = player_scene.instantiate()
-		player.set_peer_id(peer_id)
-		multiplayer_root.add_child(player, true)
+		$PlayerSpawner.spawn(peer_id)
+		
+		
+func spawn_player(data : Variant) -> Node:
+	var player : Player = player_scene.instantiate()
+	var peer_id = int(data)
+	player.set_peer_id(peer_id)
+	return player
