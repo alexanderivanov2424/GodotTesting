cabb5801a8742b0bb732b96376b6a5ab543d88a9
Jason Crowley <crowley.json@gmail.com>
2025-08-03 13:52:19 -0700

    use single spawner and spawn point

--- scripts/game.gd
+++ scripts/game.gd
@@ -3,29 +3,22 @@ extends Node
 @export var Map: PackedScene
 @export var Player: PackedScene
 
-@onready var map: Node = $Map
-@onready var players: Node = $Players
-@onready var map_spawner: MultiplayerSpawner = $MapSpawner
-@onready var player_spawner: MultiplayerSpawner = $PlayerSpawner
+@onready var spawner: MultiplayerSpawner = $Spawner
+@onready var spawn_point: Node = $SpawnPoint
 
 func _ready() -> void:
 	Lobby.lobby_ready.connect(_start_game)
 
-	player_spawner.spawn_function = _spawn_player
+	spawner.spawn_function = _spawn_player
 
 func _start_game():
-	for c in map.get_children():
-		map.remove_child(c)
+	for c in spawn_point.get_children():
+		spawn_point.remove_child(c)
 		c.queue_free()
 
-	map.add_child(Map.instantiate())
-
-	for c in players.get_children():
-		players.remove_child(c)
-		c.queue_free()
-
+	spawn_point.add_child(Map.instantiate())
 	for peer_id in Lobby.players:
-		player_spawner.spawn(peer_id)
+		spawner.spawn(peer_id)
 
 func _spawn_player(data: Variant) -> Node:
 	var peer_id := int(data)
