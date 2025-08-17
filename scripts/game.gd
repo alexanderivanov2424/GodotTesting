e2258780afd1ecd2e86a8059e298d21deedf83f7
Jason Crowley <crowley.json@gmail.com>
2025-08-16 20:18:09 -0700

    revert to setting authority in spawn_function

--- scripts/game.gd
+++ scripts/game.gd
@@ -3,17 +3,20 @@ extends Node
 @export var Map: PackedScene
 @export var Player: PackedScene
 
+@onready var spawner: MultiplayerSpawner = $Spawner
 @onready var spawn_point: Node = $SpawnPoint
 
 func _ready() -> void:
 	Lobby.lobby_ready.connect(_start_game)
 
+	spawner.spawn_function = func(peer_id: int) -> Node:
+		var player = Player.instantiate()
+		player.id = peer_id
+		player.set_multiplayer_authority(peer_id)
+		return player
+
 func _start_game():
 	spawn_point.add_child(Map.instantiate())
 
 	for peer_id in Lobby.players:
-		var player := Player.instantiate()
-		player.id = peer_id
-
-		spawn_point.add_child(player, true)
-		player.set_multiplayer_authority(peer_id)
+		spawner.spawn(peer_id)
